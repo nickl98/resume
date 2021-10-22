801642944bf6e52e057293d95a7018a6491833a2
nickl98 <nicklodde98@gmail.com>
2021-10-22 01:21:40 +0000

    Intial Commit

---
+++ assets/js/maps.js
@@ -0,0 +1,28 @@
+
+       function initMap(){
+            var map = new google.maps.Map(document.getElementById("map"),{
+                zoom: 8,
+                center: {
+                    lat: 34.034390,
+                    lng: -84.430920
+                }
+            });
+
+            var labels = "asjkhdlaskh";
+
+            var locations = [
+                {lat: 34.058180, lng: -84.379580},
+                {lat: 34.134117, lng: -118.321495},
+                {lat: 37.383064, lng: -109.071236}
+            ];
+
+            var markers = locations.map(function(location, i){
+                return new google.maps.Marker({
+                    position: location,
+                    label: labels[i % labels.lenght]
+                });
+            });
+            new MarkerClusterer(map, markers, {
+    imagePath:"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",});
+       }    
+    
