ac4ea57670f658ec790ed918147fd0dde2968f44
nickl98 <nicklodde98@gmail.com>
2022-01-18 16:13:01 +0000

    changed the hover color to footer icons

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,7 +1,7 @@
 
        function initMap(){
             var map = new google.maps.Map(document.getElementById("map"),{
-                zoom: 8,
+                zoom: 9,
                 center: {
                     lat: 34.034390,
                     lng: -84.430920
